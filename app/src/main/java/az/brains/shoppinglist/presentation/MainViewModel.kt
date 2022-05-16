fecee81dd4871c4d97208f107b1ac0cae91af3d9
AnvarZZ <anvarzeynalzade@gmail.com>
2022-05-16 19:17:12 +0400

    Creating Presentation layer 1

---
+++ app/src/main/java/az/brains/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,36 @@
+package az.brains.shoppinglist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import az.brains.shoppinglist.data.ShopListRepositoryImpl
+import az.brains.shoppinglist.domain.DeleteShopItemUseCase
+import az.brains.shoppinglist.domain.EditShopItemUseCase
+import az.brains.shoppinglist.domain.GetShopListUseCase
+import az.brains.shoppinglist.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
